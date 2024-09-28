3164be8c7b1bd42670e5d4e34b7122c783d8b775
nlhtungg <nlhtungg@gmail.com>
2024-09-28 11:47:01 +0700

    done lab1

---
+++ Lab1/Lab1_66.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Lab1_66 {
+    public static void main(String args[])
+    {
+        int a, b, x, y;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Number of rows of matrix");
+        a = input.nextInt();
+        System.out.println("Number of columns of matrix");
+        b  = input.nextInt();
+
+        int array1[][] = new int[a][b];
+        int array2[][] = new int[a][b];
+        int sum[][] = new int[a][b];
+
+        System.out.println("Input elements of first matrix");
+
+        for (  x = 0 ; x < a ; x++ )
+            for ( y = 0 ; y < b ; y++ )
+                array1[x][y] = input.nextInt();
+
+        System.out.println("Input the elements of second matrix");
+
+        for ( x = 0 ; x < a ; x++ )
+            for ( y = 0 ; y < b ; y++ )
+                array2[x][y] = input.nextInt();
+
+        for ( x = 0 ; x < a ; x++ )
+            for ( y = 0 ; y < b ; y++ )
+                sum[x][y] = array1[x][y] + array2[x][y];
+
+        System.out.println("Sum of the matrices: ");
+
+        for ( x = 0 ; x < a ; x++ )
+        {
+            for ( y = 0 ; y < b ; y++ )
+                System.out.print(sum[x][y]+"\t");
+
+            System.out.println();
+        }
+    }
+}
---
+++ Lab1/_65.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class _65 {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.println("Enter the size of array: ");
+            int s = input.nextInt();
+            int[] a = new int[s];
+            int swap;
+            for (int i=0;i<s;i++)
+            {
+                a[i] = input.nextInt();
+            }
+            for (int i=0;i<s;i++)
+            {
+                for(int j=i;j<s;j++)
+                {
+                    if (a[i]>a[j])
+                    {
+                        swap = a[i];
+                        a[i] = a[j];
+                        a[j] = swap;
+                    }
+                }
+            }
+            for (int i=0;i<s;i++)
+            {
+                System.out.print(a[i]+" ");
+            }
+        }
+    }
+}
